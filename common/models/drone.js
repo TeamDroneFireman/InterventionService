6db2604b64b1abb50e32ef357335ad357a15bb70
Gweltaz Gestin <gestin.gweltaz@gmail.com>
2016-03-21 14:20:40 +0100

    add datasource

--- common/models/drone.js
+++ common/models/drone.js
@@ -1,3 +1,28 @@
 module.exports = function(Drone) {
 
+  var find = Drone.find;
+  var cache = {};
+
+  Drone.find = function(filter, cb) {
+    var key = '';
+    if(filter) {
+      key = JSON.stringify(filter);
+    }
+    var cachedResults = cache[key];
+    if(cachedResults) {
+      console.log('serving from cache');
+      process.nextTick(function() {
+        cb(null, cachedResults);
+      });
+    } else {
+      console.log('serving from db');
+      find.call(Note, function(err, results) {
+        if(!err) {
+          cache[key] = results;
+        }
+        cb(err, results);
+      });
+    }
+  }
+
 };
