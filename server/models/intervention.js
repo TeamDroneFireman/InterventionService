41d8b69b8ef3a1f8b625623d513d7930faffb967
tremo <alextremoceiro@gmail.com>
2016-04-22 17:28:26 +0200

    Update pour le petit maxime

--- server/models/intervention.js
+++ server/models/intervention.js
@@ -1,27 +1,33 @@
 module.exports = function(Intervention) {
   const serviceName = 'Service Intervention : ';
+  const USERSERVICE_URL='http://0.0.0.0:3004/SITUsers/'
+  const http = require('http');
 
   /**
-   * Disabled auto-generated methods
+   * Disable auto-generated methods
     */
-
-  var disabledMethods = ['upsert', "deleteById", "updateAll", "updateAttributes", "createChangeStream", "exists", "find", "findOne", "count", "findById"];
+  var disabledMethods = [
+    ];
   for(method in disabledMethods){
     console.log(serviceName+"disabled auto-generated method \"" + disabledMethods[method]+"\"");
     Intervention.disableRemoteMethod(disabledMethods[method], true);
   }
-  // Due to a bug, we have to disable updateAttributes by passing false to disableRemoteMethod... tricky dev
+  // Tricky bug here (passing false to disable)
   Intervention.disableRemoteMethod('updateAttributes', false);
 
+
   /**
+   * isConnected contact an authentification api
    *
    * @param accessToken
    * @param userid
    * @returns {boolean}
      */
-  var isConnected = function (accessToken, userid){
+  var isConnecdted = function (accessToken, userid){
+    // if(isConnected(ctx.req.accessToken, ctx.req.headers.userid)
     if(userid != null && accessToken != null){
       console.log("User "+ ctx.req.headers.userid +" is connected");
+      http.get()
       return true;
     }else {
       console.log("User "+ ctx.req.headers.userid +" is not connected");
@@ -29,48 +35,47 @@ module.exports = function(Intervention) {
     }
   };
 
-  function httpGet(theUrl)
+  var httpUserServiceOptions = {
+    port: 3004,
+    hostname: '0.0.0.0',
+    method: 'GET'
+  };
+  var isConnected = function (callback, token, userid)
   {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-    xmlHttp.send( null );
-    return xmlHttp.responseText;
-  }
+    //var token = "";
+
+    httpUserServiceOptions.path=USERSERVICE_URL+userid+"/accessTokens/"+token+"?access_token="+token;
+    console.log("Calling : "+httpUserServiceOptions.path);
+    http.get(httpUserServiceOptions, function(response){
+      if(response.statusCode==200){
+        console.log("User connected");
+
+        callback(true);
+      }else{
+        console.log("User not connected");
+        callback(false);
+      }
+    }).on("error", function(e){
+      console.log("Got error: " + e.message);
+      callback(false);
+    });
+  };
 
 
   /**
    * Before every remoteMethod, check that the token correspond to the user
-    */
-
-  Intervention.beforeRemote('*', function(ctx, unused, next) {
-    if(isConnected(ctx.req.accessToken, ctx.req.headers.userid)){
-
+   */
+    Intervention.beforeRemote('*', function(ctx, unused, next) {
       next();
-    }else{
-      next(new Error('You must be logged in to access database'));
-    }
   });
 
 
   /**
-   * After every remoteMethod, check that the token correspond to the user
+   * After every remoteMethod
    */
-
   Intervention.afterRemote('find', function(ctx, modelInstance, next) {
-    if(Array.isArray(modelInstance)){
-      console.log(" modelInstance is array");
-      ctx.result.forEach(function(intervention){
-        var temp = {};
-        temp[intervention]=ctx.result[intervention];
-
-        console.log("   creationDate : " +intervention.creationDate);
-      })
-    }
     next();
   });
-
-
-
 
 };
 
