c0e56265000e42423e8304983cd03224cb078694
Gweltaz GESTIN <gestin.gweltaz@gmail.com>
2016-04-25 14:25:18 +0200

    add npm script + droneService

--- server/models/intervention.js
+++ server/models/intervention.js
@@ -1,6 +1,34 @@
 module.exports = function(Intervention) {
-  const serviceName = 'Service Intervention : ';
-  
+  Intervention.disableRemoteMethod('deleteById', true);
+  Intervention.disableRemoteMethod("updateAll", true);
+  Intervention.disableRemoteMethod('createChangeStream', true);
+  Intervention.disableRemoteMethod('findOne', true);
+  Intervention.disableRemoteMethod('exists', true);
+
+  Intervention.findByIdWithElements = function(id,cb){
+    Intervention.findById(id,function (err,intervention){
+      var droneService = Intervention.app.dataSources.droneService;
+      droneService.findById(id,function(err, response) {
+        if (err) throw err;
+        if (response.error) {
+          next('> response error: ' + response.error.stack);
+        }
+        intervention.drones = response;
+        cb(null, intervention);
+      });
+    });
+  };
+
+
+  Intervention.remoteMethod('findByIdWithElements', {
+    description: 'Find a model instance by id from the data source.',
+    accessType: 'READ',
+    accepts: [{ arg: 'id', type: 'any', description: 'Model id', required: true,
+        http: {source: 'path'}}],
+    returns: {arg: 'data', type: "intervention", root: true},
+    http: {verb: 'get', path: '/:id/WithElements'}
+  });
+
 
 
 };
