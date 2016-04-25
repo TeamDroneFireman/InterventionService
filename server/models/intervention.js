498802a6de62ff390971ecc0b4f8a68975c28baf
thomas_huchede <huchede.thomas@gmail.com>
2016-04-25 11:54:14 +0200

    Add tests and Dockerfile

--- server/models/intervention.js
+++ server/models/intervention.js
@@ -1,28 +1,30 @@
 module.exports = function(Intervention) {
-  console.log("Disabled methods");
-  Intervention.disableRemoteMethod("updateAttributes", false);       // Removes (PUT) /products/:id
-  Intervention.disableRemoteMethod('createChangeStream', true);    // removes (GET|POST) /products/change-stream
+  console.log('Disabled methods');
+  // Removes (PUT) /products/:id
+  Intervention.disableRemoteMethod('updateAttributes', false);
+  // removes (GET|POST) /products/change-stream
+  Intervention.disableRemoteMethod('createChangeStream', true);
 
   Intervention.beforeRemote('find', function(ctx, unused, next) {
-    console.log("Before remote find:");
-    console.log("Argument filter: " + ctx.args.filter);
-    // Next is needed to call find method. If you want another response, don't use it
+    console.log('Before remote find:');
+    console.log('Argument filter: ' + ctx.args.filter);
+    // Next is needed to call find method
+    // If you want another response, don't use it
     next();
   });
 
   Intervention.afterRemote('find', function(ctx, modelInstance, next) {
     // Model instance is the returned array
     if(Array.isArray(modelInstance)){
-      console.log(" modelInstance is array");
+      console.log(' modelInstance is array');
       ctx.result.forEach(function(intervention){
         var temp = {};
         temp[intervention]=ctx.result[intervention];
         // Here creationDate is a field of my intervention adapt it mtfk !! :)
-        console.log("   creationDate : " +intervention.creationDate);
-      })
+        console.log('   creationDate : ' +intervention.creationDate);
+      });
     }
     next();
   });
 
 };
-
